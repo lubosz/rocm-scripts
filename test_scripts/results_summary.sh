621dce49ee02076c214044cb505d816a3c98e945
surchundiamd <surendrababu.chundi@amd.com>
2019-05-27 12:54:58 +0530

    Update results_summary.sh

--- test_scripts/results_summary.sh
+++ test_scripts/results_summary.sh
@@ -3,46 +3,46 @@
 cd /dockerx/
 
 
-echo "\n\n\n===============MIOpen================="
+echo "\n\n===============MIOpen=================\n"
 
 timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocALUTION================="
+echo "\n\n===============rocALUTION=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocalution-test.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocPRIM================="
+echo "\n\n===============rocPRIM=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocPRIM_unittest.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============hipSPARSE================="
+echo "\n\n===============hipSPARSE=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f hipsparse-test.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocSPARSE================="
+echo "\n\n===============rocSPARSE=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocsparse-test.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocFFT================="
+echo "\n\n===============rocFFT=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocfft.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============hipBLAS================="
+echo "\n\n===============hipBLAS=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f hipblas.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocBLAS================="
+echo "\n\n===============rocBLAS=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocblas.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocRAND_Kernel_BM================="
+echo "\n\n===============rocRAND_Kernel_BM=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f bm_rocrand_kernal.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocRAND_Generate_BM================="
+echo "\n\n===============rocRAND_Generate_BM=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f bm_rocrand_generate.log >> ~/dockerx/full_summary.log
 
-echo "\n\n\n===============rocRAND================="
+echo "\n\n===============rocRAND=================\n"
 
-timeout 1s tail -f mlopen-ut.log >> ~/dockerx/full_summary.log
+timeout 1s tail -f rocrand-ut.log >> ~/dockerx/full_summary.log
