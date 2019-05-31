2e67c11803d70b8caf85597ab226002dbb59fc3d
surchundiamd <surendrababu.chundi@amd.com>
2019-05-31 15:13:19 +0530

    Update new_baremetal_tests.sh

--- test_scripts/new_baremetal_tests.sh
+++ test_scripts/new_baremetal_tests.sh
@@ -4,10 +4,12 @@ cd ~/Desktop/
 
 git clone https://streamhsa:AH64_uh1@github.com/RadeonOpenCompute/MLSEQA_TestRepo
 cd MLSEQA_TestRepo/HIP/HIPinMultiprocessing/fork
+chmod 777 -R *
 ./BuildnRun.sh
 timeout 1s tail -f Results.ini 2>&1 | tee -a ~/dockerx/full_summary.log
 
 
 cd MLSEQA_TestRepo/HIP/HIPinMultiprocessing/exec
+chmod 777 -R *
 ./BuildnRun.sh
 timeout 1s tail -f Results.ini 2>&1 | tee -a ~/dockerx/full_summary.log
