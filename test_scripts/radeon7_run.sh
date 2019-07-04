765efc15e23026e50c954b13e8085515038d0cf5
Chundi <surchundi@amd.com>
2019-07-04 13:26:08 +0530

    Create radeon7_run.sh
    for radeon 7

---
+++ test_scripts/radeon7_run.sh
@@ -0,0 +1,16 @@
+./basic-check.sh
+
+./run_hip-docker.sh
+
+./run_mathlibs-docker.sh
+./run_frameworks-docker.sh
+
+./tf-fp16-1gpu.sh
+
+./tf_tests-1gpu.sh
+
+./alexnet-flower_1gpu.sh
+
+./new_docker_tests.sh
+
+./results_summary.sh
