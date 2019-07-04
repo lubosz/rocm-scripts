63cd9cfa3c4a4bfadb6eb1e31f7ab11d27e34e0d
Chundi <surchundi@amd.com>
2019-07-04 13:25:49 +0530

    Deleted machine specific tests folder
    added the 1gpu files to test_scripts folder itself

--- machine_specific_scripts/radeon7/run_miOpen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current=`pwd`
-dir=/root/driver
-logs=/dockerx
-
-cd $dir/MLOpen/build_hip
-export MIOPEN_CONV_PRECISE_ROCBLAS_TIMING=0
-make check -j16 2>&1| tee $logs/mlopen-ut.log
-unset MIOPEN_CONV_PRECISE_ROCBLAS_TIMING
