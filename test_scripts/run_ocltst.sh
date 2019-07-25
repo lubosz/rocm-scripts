eb75b4b706d07e2c98e6ce197683fece75f7022a
avuppu <avuppu@amd.com>
2019-07-25 17:45:22 +0530

    Update run_ocltst.sh
    removed ocl-debugger from ocltst

--- test_scripts/run_ocltst.sh
+++ test_scripts/run_ocltst.sh
@@ -10,7 +10,7 @@ unzip ocltst.zip && cd ocltst/x86_64
 export LD_LIBRARY_PATH=/opt/rocm/opencl/lib/x86_64/:.
 
 ./ocltst -m oclregression.so -A oclregression.exclude 2>&1 | tee -a ~/dockerx/ocltst/oclregression.log
-./ocltst -m ocldebugger.so -A ocldebugger.exclude 2>&1 | tee -a ~/dockerx/ocltst/ocldebugger.log
+#./ocltst -m ocldebugger.so -A ocldebugger.exclude 2>&1 | tee -a ~/dockerx/ocltst/ocldebugger.log
 ./ocltst -m oclmediafunc.so -A oclmediafunc.exclude 2>&1 | tee -a ~/dockerx/ocltst/oclmediafunc.log
 ./ocltst -m oclprofiler.so -A oclprofiler.exclude 2>&1 | tee -a ~/dockerx/ocltst/oclprofiler.log
 ./ocltst -m oclcompiler.so -A oclcompiler.exclude 2>&1 | tee -a ~/dockerx/ocltst/oclcompiler.log
