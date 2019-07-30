9903128987c7fbfd540feccc42dbc796a97d3401
Chundi <surchundi@amd.com>
2019-07-30 11:44:47 +0530

    Update new_baremetal_tests.sh

--- test_scripts/new_baremetal_tests.sh
+++ test_scripts/new_baremetal_tests.sh
@@ -13,3 +13,13 @@ cd MLSEQA_TestRepo/HIP/HIPinMultiprocessing/exec
 chmod 777 -R *
 ./BuildnRun.sh
 timeout 1s tail -f Results.ini 2>&1 | tee -a ~/dockerx/full_summary.log
+
+#rocm-profiler
+
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_profiler/env_file?token=AB7ZUFVMLAQEVBHQE5DLDKS5JEMYG -O env_file
+
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_profiler/rcp_build.sh?token=AB7ZUFXUI3KNOVWLEMTZASS5JEMYS -O rcp_build.sh
+
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_profiler/rcp_tests.py?token=AB7ZUFS73X4JYZOFBT55ODK5JEMYY -O rcp_tests.py
+
+python rcp_tests.py
