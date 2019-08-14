496f2fc49d09ec35ccd5b66365ebcbed9842e880
Chundi <surchundi@amd.com>
2019-08-14 11:29:27 +0530

    Update new_docker_tests.sh

--- test_scripts/new_docker_tests.sh
+++ test_scripts/new_docker_tests.sh
@@ -1,18 +1,17 @@
 sudo apt-get install python-prettytable
 
-cd /root/driver/
+cd /dockerx/
 
-git clone https://streamhsa:AH64_uh1@github.com/RadeonOpenCompute/MLSEQA_TestRepo
+git pull https://streamhsa:AH64_uh1@github.com/RadeonOpenCompute/MLSEQA_TestRepo
 cd MLSEQA_TestRepo/Libs/hipCUB
 python hipCUB.py 2>&1 | tee -a /dockerx/hipCUB.log
 
 
-cd /root/driver/MLSEQA_TestRepo/Libs/rocThrust
+cd /dockerx/MLSEQA_TestRepo/Libs/rocThrust
 python rocThrust.py 2>&1 | tee -a /dockerx/rocThrust.log
 
-./run_ocltst.sh
-
 "
+./run_ocltst.sh
 
 cd /root/driver/
 git clone https://github.com/ROCmSoftwarePlatform/rccl.git && cd rccl
@@ -29,5 +28,10 @@ sudo dpkg -i rccl*.deb
 cd /root/driver/
 
 git clone -b master https://github.com/ROCmSoftwarePlatform/rccl-tests.git && cd rccl-tests
+
+
+git clone https://streamhsa:AH64_uh1@github.com/RadeonOpenCompute/MLSEQA_TestRepo
+
+wget https://raw.githubusercontent.com/raspberrypi/linux/rpi-4.9.y/arch/arm/configs/bcmrpi_defconfig
 
 "
