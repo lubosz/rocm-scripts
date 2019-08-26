8e092a2f89f58938cf136b1fc288a2e10754c826
Chundi <surchundi@amd.com>
2019-08-26 13:03:39 +0530

    Update rocm_smi_all.sh

--- test_scripts/rocm_smi_all.sh
+++ test_scripts/rocm_smi_all.sh
@@ -3,10 +3,10 @@ echo AH64_uh1 | sudo -S rm -rf rocm_smi_tool
 
 mkdir -p ~/dockerx/rocm_smi_tool && chmod 777 -R rocm_smi_tool && cd ~/dockerx/rocm_smi_tool
 
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_common.py?token=AB7ZUFXBZ3MX6PPFH4AWVW25HGJNM -O rocm_smi_common.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_sanity.py?token=AB7ZUFTHXMOIUH3LO3X4MUC5HGK42 -O rocm_smi_sanity.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_system_info.py?token=AB7ZUFWUL5IWDR6A4HWDNY25HGK64 -O rocm_smi_system_info.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_workload_tests.py?token=AB7ZUFQ3H3A5FJFDLGKEGKC5HGLBS -O rocm_smi_workload_tests.py
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_common.py?token=AB7ZUFUDFAMZVCOMZCAPYBC5JVHWU -O rocm_smi_common.py
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_sanity.py?token=AB7ZUFXZIGQZB6ROFFDO6NK5JVHW4 -O rocm_smi_sanity.py
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_system_info.py?token=AB7ZUFX6ZMGUJ7Q3B7EA5DC5JVHXG -O rocm_smi_system_info.py
+wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_workload_tests.py?token=AB7ZUFUWFYU6BCF45CG47BK5JVHXS -O rocm_smi_workload_tests.py
 
 echo AH64_uh1 | sudo -S python3 rocm_smi_sanity.py
 cp rocm_smi_sanity.summary ~/dockerx/
