98b17f0e2f3f503d871edeb9a7aa858671f2fc12
surchundiamd <surendrababu.chundi@amd.com>
2019-09-24 18:05:41 +0530

    Update rocm_smi_all.sh

--- test_scripts/rocm_smi_all.sh
+++ test_scripts/rocm_smi_all.sh
@@ -1,15 +1,71 @@
-cd ~/dockerx/
-echo AH64_uh1 | sudo -S rm -rf rocm_smi_tool
+#!/bin/bash
 
-mkdir -p ~/dockerx/rocm_smi_tool && chmod 777 -R rocm_smi_tool && cd ~/dockerx/rocm_smi_tool
+logs=~/dockerx/
 
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_common.py?token=AB7ZUFUDFAMZVCOMZCAPYBC5JVHWU -O rocm_smi_common.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_sanity.py?token=AB7ZUFXZIGQZB6ROFFDO6NK5JVHW4 -O rocm_smi_sanity.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_system_info.py?token=AB7ZUFX6ZMGUJ7Q3B7EA5DC5JVHXG -O rocm_smi_system_info.py
-wget https://raw.githubusercontent.com/RadeonOpenCompute/MLSEQA_TestRepo/master/ROCM_Tools/rocm_smi_tool/rocm_smi_workload_tests.py?token=AB7ZUFUWFYU6BCF45CG47BK5JVHXS -O rocm_smi_workload_tests.py
+################ Update Git Repo in ~/dockerx/MLSEQA_TestRepo/ #######################
 
-echo AH64_uh1 | sudo -S python3 rocm_smi_sanity.py
-cp rocm_smi_sanity.summary ~/dockerx/
+rm -rf $logs/ROCM_Tools/
 
-echo AH64_uh1 | sudo -S python3 rocm_smi_workload_tests.py
-cp rocm_smi_workloads.summary ~/dockerx/
+cd ~/dockerx/MLSEQA_TestRepo/
+
+git stash
+
+git pull https://streamhsa:AH64_uh1@github.com//RadeonOpenCompute/MLSEQA_TestRepo
+
+cp -rf ROCM_Tools $logs
+
+cd $logs/ROCM_Tools/
+
+chmod 777 -R *
+
+################ start rocm_smi_lib_test #######################
+
+cd $logs/ROCM_Tools/rocm_smi_lib_test
+
+sudo python smi_lib_app_test.py
+
+cp -rf smi_lib_app_test.summary $logs
+cp -rf smi_lib_app_test_github_func.log $logs
+cp -rf smi_lib_app_test_unit.log $logs
+cp -rf smi_lib_app_test_stress.log $logs
+
+################ start rocprofiler #######################
+
+cd $logs/ROCM_Tools/rocprofiler
+
+sudo python3 rocprofiler_tests.py
+
+cp -rf rocprofiler_tests.summary $logs
+cp -rf rocprofiler_tests.log $logs
+
+################ start rocm_smi_sanity #######################
+
+cd $logs/ROCM_Tools/rocm_smi_tool
+
+sudo python3 rocm_smi_sanity.py
+
+cp -rf rocm_smi_sanity.summary $logs
+cp -rf rocm_smi_sanity.log $logs
+
+################ start rocm_smi_workload_tests #######################
+
+cd $logs/ROCM_Tools/rocm_smi_tool
+
+sudo python3 rocm_smi_workload_tests.py
+
+cp -rf rocm_smi_workload_tests.summary $logs
+cp -rf rocm_smi_workload_tests.log $logs
+
+cd $logs
+
+echo '################### rocm_smi_workload_tests ##########################'
+cat rocm_smi_workload_tests.summary
+
+echo '################### rocm_smi_sanity ##########################'
+cat rocm_smi_sanity.summary
+
+echo '################### rocprofiler_tests ##########################'
+cat rocprofiler_tests.summary
+
+echo '################### smi_lib_app_test ##########################'
+cat smi_lib_app_test.summary
