47fb5405f8402ef7deb3c335a69ecdff2e4b50fd
surchundiamd <surendrababu.chundi@amd.com>
2019-09-24 18:02:51 +0530

    Update run_mathlibs-docker.sh

--- jenkins-scripts/run_mathlibs-docker.sh
+++ jenkins-scripts/run_mathlibs-docker.sh
@@ -40,9 +40,10 @@ sudo apt-get install python-prettytable
 cd /dockerx/
 
 git pull https://streamhsa:AH64_uh1@github.com/RadeonOpenCompute/MLSEQA_TestRepo
+
 cd MLSEQA_TestRepo/Libs/hipCUB
-python hipCUB.py 2>&1 | tee -a /dockerx/hipCUB.log
+sudo python hipCUB.py 2>&1 | tee -a /dockerx/hipCUB.log
 
 
 cd /dockerx/MLSEQA_TestRepo/Libs/rocThrust
-python rocThrust.py 2>&1 | tee -a /dockerx/rocThrust.log
+sudo python rocThrust.py 2>&1 | tee -a /dockerx/rocThrust.log
