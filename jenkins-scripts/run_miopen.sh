9b59d2b98e7e708d94373d8354b6854bcebbe54e
Chundi <surchundi@amd.com>
2019-12-17 20:20:07 +0530

    Update run_miopen.sh

--- jenkins-scripts/run_miopen.sh
+++ jenkins-scripts/run_miopen.sh
@@ -3,33 +3,6 @@ current=`pwd`
 dir=/root/driver
 logs=/dockerx
 
-#============MIOpen_OCL===================
-
-sudo dpkg -r miopen-hip
-
-cd $dir/MLOpen/
-
-mkdir -p build_ocl && cd build_ocl
-
-rm -rf *
-
-#To build MIOpen with OpenCL backend 
-cmake -DMIOPEN_TEST_ALL=ON -DMIOPEN_BACKEND=OpenCL -DMIOPEN_MAKE_BOOST_PUBLIC=ON -DBoost_USE_STATIC_LIBS=Off -DMIOPEN_TEST_FLAGS="--disable-verification-cache" -DOPENCL_INCLUDE_DIRS=/opt/rocm/opencl/include/ -DOPENCL_LIBRARIES=/opt/rocm/opencl/lib/x86_64/libamdocl64.so .. | tee -a $logs/mlopenocl_build.log
-
-make -j$(nproc) | tee -a $logs/mlopenocl_build.log
-make package | tee -a $logs/mlopenocl_build.log
-echo AH64_uh1 | sudo dpkg -i *.deb
-
-
-export MIOPEN_CONV_PRECISE_ROCBLAS_TIMING=0
-make check -j$(nproc) 2>&1| tee $logs/mlopen-ut-ocl.log
-unset MIOPEN_CONV_PRECISE_ROCBLAS_TIMING
-
-sudo dpkg -r miopen-opencl
-
-cd $dir/MLOpen/build_hip
-echo AH64_uh1 | sudo dpkg -i *.deb
-
 #============MIOpen_HIP===================
 
 sudo dpkg -r miopen-hip
@@ -61,3 +34,31 @@ export MIOPEN_CONV_PRECISE_ROCBLAS_TIMING=0
 ./MIOpenDriver conv -n 1 -c 512 -H 256 -W 256 -k 6 -y 1 -x 1 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1 -m conv -g 1 -t 1 2>&1 | tee -a $logs/MIOpenDriver_run.log
 ./MIOpenDriver conv -n 1 -c 64 -H 130 -W 130 -k 3 -y 3 -x 3 -p 0 -q 0 -u 1 -v 1 -l 1 -j 1 -m conv -g 1 -t 1 2>&1 | tee -a $logs/MIOpenDriver_run.log
 unset MIOPEN_CONV_PRECISE_ROCBLAS_TIMING
+
+
+#============MIOpen_OCL===================
+
+sudo dpkg -r miopen-hip
+
+cd $dir/MLOpen/
+
+mkdir -p build_ocl && cd build_ocl
+
+rm -rf *
+
+#To build MIOpen with OpenCL backend 
+cmake -DMIOPEN_TEST_ALL=ON -DMIOPEN_BACKEND=OpenCL -DMIOPEN_MAKE_BOOST_PUBLIC=ON -DBoost_USE_STATIC_LIBS=Off -DMIOPEN_TEST_FLAGS="--disable-verification-cache" -DOPENCL_INCLUDE_DIRS=/opt/rocm/opencl/include/ -DOPENCL_LIBRARIES=/opt/rocm/opencl/lib/x86_64/libamdocl64.so .. | tee -a $logs/mlopenocl_build.log
+
+make -j$(nproc) | tee -a $logs/mlopenocl_build.log
+make package | tee -a $logs/mlopenocl_build.log
+echo AH64_uh1 | sudo dpkg -i *.deb
+
+
+export MIOPEN_CONV_PRECISE_ROCBLAS_TIMING=0
+make check -j$(nproc) 2>&1| tee $logs/mlopen-ut-ocl.log
+unset MIOPEN_CONV_PRECISE_ROCBLAS_TIMING
+
+sudo dpkg -r miopen-opencl
+
+cd $dir/MLOpen/build_hip
+echo AH64_uh1 | sudo dpkg -i *.deb
